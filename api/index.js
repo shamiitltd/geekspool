628b131a31d8983a4df457e6d9cb5a8f02a8bfd8
Naresh Upadhyay <naresh.king88898@gmail.com>
2022-11-02 00:34:33 +0530

    updated

--- api/index.js
+++ api/index.js
@@ -1,34 +1,27 @@
-const passport = require('passport');
-const {
-    initializePassport
-} = require('./routes/libraries/passportFunctions');
-const {
-    checkAuthentication,
-    checkNotAuthenticated
-} = require('./routes/libraries/normal_functions');
+const express = require('express');
+const api = express.Router();
+
+const verify = require('../middleware/verify');
 
 
-// app.use('/variablesData', variablesData); // Routing path
-app.use('/', getRoutes); // Routing path 
-
-app.get('/signin', checkNotAuthenticated, async (req, res) => {
+api.get('/signin', verify.checkNotAuthenticated, async (req, res) => {
     res.render('outerMostContainers/containerWithSearchForm', {
         user: ''
     });
 })
-app.get('/offline', async (req, res) => {
+api.get('/offline', async (req, res) => {
     res.render('outerMostContainers/containerWithOffline', {
         user: req.user
     });
 })
 
-app.get('/help', (req, res) => {
+api.get('/help', (req, res) => {
     res.render('landing/help');
 })
 
 
 //set Different routes
-app.get('/', async (req, res) => {
+api.get('/', async (req, res) => {
     res.render('outerMostContainers/containerWithSearchForm', {
         user: req.user
     });
@@ -36,13 +29,13 @@ app.get('/', async (req, res) => {
 
 
 
-app.get('/recruiters', (req, res) => {
+api.get('/recruiters', (req, res) => {
     res.render('outerMostContainers/containerWithSearchForm', {
         user: req.user
     });
 })
 
-app.get('/courses', async (req, res) => {
+api.get('/courses', async (req, res) => {
     let {
         searchAll
     } = req.query;
@@ -54,7 +47,7 @@ app.get('/courses', async (req, res) => {
     });
 })
 
-app.get('/courses/search', async (req, res) => {
+api.get('/courses/search', async (req, res) => {
     let {
         searchAll
     } = req.query;
@@ -65,7 +58,7 @@ app.get('/courses/search', async (req, res) => {
         user: req.user
     });
 })
-app.get('/viewcourses', async (req, res) => {
+api.get('/viewcourses', async (req, res) => {
     let {
         searchAll
     } = req.query;
@@ -84,23 +77,23 @@ app.get('/courses/organisation/:companyName/:jobtitle', async (req, res) => { //
     });
 })
 
-app.get('/*/new', checkAuthentication, (req, res) => {
+app.get('/*/new', verify.checkAuthentication, (req, res) => {
     res.render('outerMostContainers/containerWithSearchForm', {
         user: req.user
     });
 })
-app.get('/profile/*', checkAuthentication, (req, res) => {
+app.get('/profile/*', verify.checkAuthentication, (req, res) => {
     res.render('outerMostContainers/containerWithSearchForm', {
         user: req.user
     });
 })
 
-app.get('/*/edit', checkAuthentication, (req, res) => {
+app.get('/*/edit', verify.checkAuthentication, (req, res) => {
     res.render('outerMostContainers/containerWithSearchForm', {
         user: req.user
     });
 })
-app.get('/deleteInfo', checkAuthentication, (req, res) => {
+app.get('/deleteInfo', verify.checkAuthentication, (req, res) => {
     res.render('outerMostContainers/containerWithSearchForm', {
         user: req.user
     });
