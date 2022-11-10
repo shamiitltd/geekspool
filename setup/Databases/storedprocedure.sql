68901f282a663dec32ab1a882f7123701acef22a
Naresh Upadhyay <naresh.king88898@gmail.com>
2022-11-10 11:54:23 +0530

    updated.

--- setup/Databases/storedprocedure.sql
+++ setup/Databases/storedprocedure.sql
@@ -38,7 +38,6 @@ DROP PROCEDURE IF EXISTS Get_RssRecord_Profile;
 DELIMITER $$
 CREATE PROCEDURE Get_RssRecord_Profile(IN searchAll TEXT, IN iuserid Varchar(20), IN admin Boolean, IN lim INT, IN pageNumber INT, OUT totalposts INT)
 BEGIN
-    DECLARE totalposts INT;
     DECLARE offsetVal INT;
     SET offsetVal = (lim * (pageNumber - 1));
     if (!STRCMP(searchAll,"") = 0) then
@@ -69,39 +68,39 @@ DELIMITER ;
 /* Tool form load*/
 DROP PROCEDURE IF EXISTS Get_RssForm_load;
 DELIMITER $$
-CREATE PROCEDURE Get_RssForm_load(IN rssid Varchar(20), IN iuserid Varchar(20), IN admin Boolean)
+CREATE PROCEDURE Get_RssForm_load(IN irssid Varchar(20), IN iuserid Varchar(20), IN admin Boolean)
 BEGIN
     SELECT * FROM  
-    smaptorss WHERE rssid=rssid AND (userid=iuserid or admin IS TRUE);
+    smaptorss WHERE rssid=irssid AND (userid=iuserid or admin IS TRUE);
 END$$
 DELIMITER ;
 
 /*Update Rss table */
 DROP PROCEDURE IF EXISTS Upload_rss_InfoData;
 DELIMITER $$
-CREATE PROCEDURE Upload_rss_InfoData(IN rssid Varchar(20), IN iuserid Varchar(20), IN emails Varchar(100), IN urls Varchar(200),
-                                     IN included TEXT, IN excluded TEXT, IN remarks TEXT, IN directorypath Varchar(200),
-                                     IN language char(10), IN frequency INT, IN ndtype char(5), IN updateData Boolean)
+CREATE PROCEDURE Upload_rss_InfoData(IN irssid Varchar(20), IN iuserid Varchar(20), IN iemails Varchar(100), IN iurls Varchar(200),
+                                     IN iincluded TEXT, IN iexcluded TEXT, IN iremarks TEXT, IN idirectorypath Varchar(200),
+                                     IN ilanguage char(10), IN ifrequency INT, IN indtype char(5), IN updateData Boolean)
 BEGIN
     if (updateData) then
     	/* Update Data in smaptorss */
         UPDATE smaptorss 
         SET userid=iuserid,
-            emails=emails,
-            urls=urls,
-            included=included,
-            excluded=excluded,
-            remarks=remarks,
-            language=language,
-            frequency=frequency,
-            ndtype=ndtype,
-            updated = DATE_ADD( CURRENT_TIMESTAMP(), INTERVAL IF(frequency != 0 , 1400/frequency, 5256000) MINUTE )
-        WHERE rssid=rssid;	
+            emails=iemails,
+            urls=iurls,
+            included=iincluded,
+            excluded=iexcluded,
+            remarks=iremarks,
+            language=ilanguage,
+            frequency=ifrequency,
+            ndtype=indtype,
+            updated = DATE_ADD( CURRENT_TIMESTAMP(), INTERVAL IF(ifrequency != 0 , 1400/ifrequency, 5256000) MINUTE )
+        WHERE rssid=irssid;	
     else 
 	    /* Insert the data into smaptorss*/
 	    INSERT INTO 
         smaptorss( rssid, userid, emails, urls, included, excluded, remarks, directorypath, language, frequency, ndtype)
-        VALUES( rssid, iuserid, emails, urls, included, excluded, remarks, directorypath, language, frequency, ndtype);
+        VALUES( irssid, iuserid, iemails, iurls, iincluded, iexcluded, iremarks, idirectorypath, ilanguage, ifrequency, indtype);
     end if;
 
 END$$
@@ -110,10 +109,10 @@ DELIMITER ;
 /* Delete from Rss table*/
 DROP PROCEDURE IF EXISTS Delete_ByRssId;
 DELIMITER $$
-CREATE PROCEDURE Delete_ByRssId(IN rssid Varchar(20))
+CREATE PROCEDURE Delete_ByRssId(IN irssid Varchar(20))
 BEGIN
     DELETE FROM smaptorss 
-    WHERE rssid=rssid;
+    WHERE rssid=irssid;
 END$$
 DELIMITER ;
 
@@ -121,29 +120,29 @@ DELIMITER ;
 /* load Drop Down Data for Rss table*/
 DROP PROCEDURE IF EXISTS Get_dropDown_Details;
 DELIMITER $$
-CREATE PROCEDURE Get_dropDown_Details(IN name Varchar(50))
+CREATE PROCEDURE Get_dropDown_Details(IN iname Varchar(50))
 BEGIN
     SELECT * FROM  dropDownsPool
-    WHERE name=name;
+    WHERE name=iname;
 END$$
 DELIMITER ;
 
 DROP PROCEDURE IF EXISTS Upload_dropDown_Details;
 DELIMITER $$
-CREATE PROCEDURE Upload_dropDown_Details(IN name Varchar(20), IN key Varchar(20), IN value Varchar(100), IN updateData Boolean)
+CREATE PROCEDURE Upload_dropDown_Details(IN iname Varchar(20), IN ikey Varchar(20), IN ivalue Varchar(100), IN updateData Boolean)
 BEGIN
     if (updateData) then
     	/* Update Data in dropDownsPool */
         UPDATE dropDownsPool 
-        SET key=key,
-            value=value,
+        SET key=ikey,
+            value=ivalue,
             updated=CURRENT_TIMESTAMP()
-        WHERE name=name;	
+        WHERE name=iname;	
     else 
 	    /* Insert the data into dropDownsPool*/
 	    INSERT INTO 
         dropDownsPool( name, key, value)
-        VALUES( name, key, value);
+        VALUES( iname, ikey, ivalue);
     end if;
 END$$
 DELIMITER ;
