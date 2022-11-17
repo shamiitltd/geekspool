b479f27019ffc5b335daa2cb2c2706bfd1e8dc23
SHAMIIT <shamiitltd@gmail.com>
2022-11-18 02:11:25 +0530

    updated

--- api/auth.js
+++ api/auth.js
@@ -31,9 +31,62 @@ routes.get('/auth/linkedin/callback', function (req, res, next) {
     callbackAuthenticator('linkedin', req, res, next);
 });
 
+routes.get('/resetpassword/:email', async (req, res) => {
+    const {
+        email
+    } = await req.params;
+    sendresetpassmail(email, res);
+})
+
+routes.get('/authresetpass', async (req, res) => {
+    const {
+        email,
+        token
+    } = req.query;
+    if (email && token) {
+        const fileLocation = 'boxes/dataUploadForms/resetpass';
+        resetPassWithEmail(fileLocation, email, token, res);
+    } else {
+        res.redirect('/nopage');
+    }
+})
+
+
 routes.get('/signOutPath', async (req, res) => {
     req.logOut();
     res.redirect('/signin');
 })
+
+routes.post('/signinUser', function (req, res, next) {
+    passport.authenticate('local', function (err, user, info) {
+        if (err) {
+            return res.send(info.message);
+        }
+        if (!user) {
+            return res.send(info.message);
+            // return res.redirect( '/signin' );
+        }
+        req.logIn(user, function (err) {
+            if (err) {
+                return res.send('Authentication error');
+            }
+            return res.send('success');
+            // return res.redirect( '/users/' + user.username );
+        });
+    })(req, res, next);
+});
+
+routes.post('/registerNewUser', async (req, res) => {
+    let dataObject = req.body;
+    dataObject.password = await bcrypt.hash(req.body.password, saltRounds);
+    registerUserFunction(dataObject, res);
+})
+
+routes.post('/resetNewPassword', async (req, res) => {
+    let dataObject = req.body;
+    dataObject.password = await bcrypt.hash(req.body.password, saltRounds);
+    resetNewPasswordFunction(dataObject, res);
+})
+
 
 module.exports = routes;
--- model/auth.js
+++ model/auth.js
@@ -27,3 +27,119 @@ exports.callbackAuthenticator =  function (provider, req, res, next) {
 }
 
 
+function registerUserFunction(dataObject, res) {
+    // console.log( dataObject );
+    let usersQueryString = `INSERT INTO 
+                        usersBasicInfo( id, name, email, password, provider )
+                        VALUES( '${dataObject.id}', '${dataObject.name}', '${dataObject.email}', '${dataObject.password}', 'offcampuscareer' )
+                        `;
+    usersDB.query(usersQueryString, (err, results, fields) => {
+        if (err) {
+            // console.log( "Not connected !!! " + err );
+            return res.send('User already exists with this email');
+        }
+
+        // console.log( 'The Inserted in table is: \n', results, fields );
+        return res.send('success');
+    });
+}
+function resetNewPasswordFunction(dataObject, res) {
+    // console.log( dataObject );
+    let usersQueryString = `SELECT * FROM usersBasicInfo WHERE email = \'${dataObject.email}\'`;
+    usersDB.query(usersQueryString, (err, results, fields) => {
+        if (err) {
+            return res.send('Some error, login with another method with different email');
+        }
+        if (!results || !results[0]) {
+            return res.send('No user with this email');
+        } else if (results[0].provider !== 'offcampuscareer') {
+            return res.send(`Already exists with ${results[0].provider} Sign In method`);
+        } else if (results[0].password !== dataObject.token) {
+            return res.send('Password already updated with this link or outdated link');
+        }
+        let usersQueryString2 = `UPDATE usersBasicInfo 
+                        SET password = '${dataObject.password}'
+                        WHERE email = \'${dataObject.email}\'
+                        `;
+        usersDB.query(usersQueryString2, (err, results2, fields) => {
+            if (err) {
+                return res.send('Some error, Please sign in with another method');
+            }
+            return res.send('success');
+        });
+    });
+}
+
+function sendresetpassmail(email, res) {
+    let usersQueryString = `SELECT * FROM usersBasicInfo WHERE email = \'${email}\'`;
+    usersDB.query(usersQueryString, (err, results, fields) => {
+        if (err) {
+            return res.send('Some error, login with another method with different email');
+        }
+        if (!results || !results[0]) {
+            return res.send('No user with this email');
+        } else if (results[0].provider !== 'offcampuscareer') {
+            return res.send(`Already exists with ${results[0].provider} Sign In method`);
+        }
+        let token = results[0].password;
+        let messgeText = `Hello ${results[0].name},
+
+Follow this link to reset your Offcampuscareer password for your ${email} account.
+
+https: //offcampuscareer.com/auth/resetpass?email=${email}&token=${token}
+
+If you didn’t ask to reset your password, you can ignore this email.
+
+Thanks,
+
+Your Offcampuscareer team`;
+        let messageHtml = `<p><strong>Hello ${results[0].name},</strong></p>
+
+<p>Follow this link to reset your Offcampuscareer password for your ${email} account.</p>
+
+<p><a href="https://offcampuscareer.com/auth/resetpass?email=${email}&token=${token}">https://offcampuscareer.com/auth/resetpass?email=${email}&token=${token}</a></p>
+
+<p>If you didn&rsquo;t ask to reset your password, you can ignore this email.</p>
+
+<p><strong>Thanks,</strong></p>
+
+<p><strong>Your Offcampuscareer team</strong></p>
+`;
+        sendMailResetPassWithEmail(results[0].email, messgeText, messageHtml, res);
+    });
+}
+
+function resetPassWithEmail(fileLocation, email, token, res) {
+    let usersQueryString = `SELECT * FROM usersBasicInfo WHERE email = \'${email}\'`;
+    usersDB.query(usersQueryString, (err, results, fields) => {
+        if (err) {
+            return res.render(fileLocation, {
+                email,
+                token,
+                message: 'Some error, Please login with another method with different email'
+            });
+        }
+        if (!results || !results[0]) {
+            return res.render(fileLocation, {
+                email,
+                token,
+                message: 'No user with this email'
+            });
+        }
+        if (token == results[0].password) {
+            return res.render(fileLocation, {
+                email,
+                token,
+                message: ''
+            });
+        } else {
+            return res.render(fileLocation, {
+                email,
+                token,
+                message: 'Password already updated with this link or outdated link'
+            });
+        }
+    });
+}
+
+
