9753f64720875b20cc18a286eed6bf09863c0d52
Naresh Upadhyay <naresh.king88898@gmail.com>
2022-11-25 01:54:02 +0530

    updated

--- api/form.js
+++ api/form.js
@@ -1,14 +1,15 @@
 const express = require('express');
+const { admin, viewer, editor } = require('../middleware/roles');
 const api = express.Router();
 const { checkAuthentication } = require('../middleware/verify');
 const { toolsUiLoader, uploadtoolInfoData, deleteRssfromDbNFile } = require('../model/profile');
 
-api.get('/rss/new', (req, res) => {
+api.get('/rss/new', checkAuthentication, [admin, editor, viewer], (req, res) => {
     const queryString = req.query;
-    toolsUiLoader(req, res, 'controllers/rssController', queryString.id);
+    toolsUiLoader(req, res, 'controllers/tools/rssgenController', queryString.id);
 })
 
-api.post('/generaterss', (req, res) => {
+api.post('/generaterss', checkAuthentication,(req, res) => {
     const dataObject = req.body;
     uploadtoolInfoData(res, dataObject);
 })
--- middleware/roles.js
+++ middleware/roles.js
@@ -1,22 +1,41 @@
 function admin(req, res, next) {
-    if (!req.user.roles.includes("admin")) return res.status(403).send({
-        ok: false,
-        error: "Admin access denied"
-    });
+    if (!req.user.role.includes("admin"))
+        req.admin = true;
+    else req.admin = false;
     next();
 }
 function editor(req, res, next) {
-    if (!req.user.roles.includes("editor")) return res.status(403).send({
-        ok: false,
-        error: "Editor access denied"
-    });
+    if (!req.user.role.includes("editor"))
+        req.editor = true;
+    else req.editor = false;
     next();
 }
 function viewer(req, res, next) {
-    if (!req.user.roles.includes("viewer")) return res.status(403).send({
-        ok: false,
-        error: "Viewer access denied"
-    });
+    if (!req.user.role.includes("viewer"))
+        req.viewer = true;
+    else req.viewer = false;
     next();
 }
 module.exports = { admin, editor, viewer };
+//function admin(req, res, next) {
+//    if (!req.user.role.includes("admin")) return res.status(403).send({
+//        ok: false,
+//        error: "Admin access denied"
+//    });
+//    next();
+//}
+//function editor(req, res, next) {
+//    if (!req.user.role.includes("editor")) return res.status(403).send({
+//        ok: false,
+//        error: "Editor access denied"
+//    });
+//    next();
+//}
+//function viewer(req, res, next) {
+//    if (!req.user.role.includes("viewer")) return res.status(403).send({
+//        ok: false,
+//        error: "Viewer access denied"
+//    });
+//    next();
+//}
+//module.exports = { admin, editor, viewer };
